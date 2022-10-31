e4b5ba2b04e5f333d2b269e51af2966bcf55c3b6
ACE1960 <113360654+ACE1960@users.noreply.github.com>
2022-10-31 16:34:14 +0100

    Gui

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import gui_main.GUI;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    GUI gui=new GUI();
 }
