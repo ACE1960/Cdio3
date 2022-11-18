8562f3b2bcdc68288eb267155beaed447a1230a1
mathias124 <mfs2001@hotmail.dk>
2022-11-18 12:40:16 +0100

    Hold  Shelved.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,7 +14,7 @@ public class Main {
          * Referer til game.play() som er et true loop, der sørger for at spillet løber indtil der er vundet.
          */
         game.play();
-
+//Hold
     }
 }
 
