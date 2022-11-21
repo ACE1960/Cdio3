e9b5cbad7e135eb23a4f09dea0703df1b83e8554
mathias124 <mfs2001@hotmail.dk>
2022-11-21 13:57:30 +0100

    Update UI

--- src/main/java/org/example/GameController.java
+++ src/main/java/org/example/GameController.java
@@ -6,6 +6,7 @@ import gui_fields.GUI_Player;
 import gui_main.GUI;
 
 
+import java.awt.*;
 import java.util.Scanner;
 
 public class GameController {
@@ -42,11 +43,11 @@ public class GameController {
 
 
         if(x==2) {
-            GUI_Car car1 = new GUI_Car();
+            GUI_Car car1 = new GUI_Car(Color.BLUE, Color.BLACK, GUI_Car.Type.CAR, GUI_Car.Pattern.HORIZONTAL_DUAL_COLOR);
             GUI_Player play1 = new GUI_Player("Player1",20,  car1);
             gui.addPlayer(play1);
 
-            GUI_Car car2 = new GUI_Car();
+            GUI_Car car2 = new GUI_Car(Color.RED, Color.RED, GUI_Car.Type.TRACTOR, GUI_Car.Pattern.HORIZONTAL_DUAL_COLOR);
             GUI_Player play2 = new GUI_Player("Player2",20,  car2);
             gui.addPlayer(play2);
 
@@ -64,6 +65,7 @@ public class GameController {
                 System.out.println(m);
                 board2[m-holder.getSum()].removeAllCars();
                 board2[m].setCar(play1,true);
+
 
                 //board2[m].setCar(play1,true);
 
@@ -93,71 +95,119 @@ public class GameController {
             }
         }
         else if (x==3){
-            GUI_Car car1 = new GUI_Car();
-            GUI_Player play1 = new GUI_Player("Player1",20,  car1);
+            GUI_Car car1 = new GUI_Car(Color.RED, Color.RED, GUI_Car.Type.TRACTOR, GUI_Car.Pattern.HORIZONTAL_DUAL_COLOR);
+            GUI_Player play1 = new GUI_Player("Player1",18,  car1);
             gui.addPlayer(play1);
 
-            GUI_Car car2 = new GUI_Car();
-            GUI_Player play2 = new GUI_Player("Player2",20,  car2);
+            GUI_Car car2 = new GUI_Car(Color.YELLOW, Color.WHITE, GUI_Car.Type.UFO, GUI_Car.Pattern.HORIZONTAL_DUAL_COLOR);
+            GUI_Player play2 = new GUI_Player("Player2",18,  car2);
             gui.addPlayer(play2);
 
-            GUI_Car car3 = new GUI_Car();
-            GUI_Player play3 = new GUI_Player("Player2",20,  car2);
+            GUI_Car car3 = new GUI_Car(Color.BLUE, Color.WHITE, GUI_Car.Type.CAR, GUI_Car.Pattern.HORIZONTAL_DUAL_COLOR);
+            GUI_Player play3 = new GUI_Player("Player3",18,  car2);
             gui.addPlayer(play3);
 
             player1 = new Player(18);
             player2= new Player(18);
             player3 = new Player(18);
-            while (playing) {
-                System.out.println(gui.getUserButtonPressed(inputText, new String[]{"Tryk!"}));
+
+                while (playing) {
+                    gui.showMessage("Player1 tryk enter:");
+                    gui.getUserButtonPressed(inputText, new String[]{"Tryk!"});
+
+                    p1+=holder.sum();
+                    gui.setDice(holder.die1.getFacevalue(),holder.die2.getFacevalue());
+                    int m=p1%24;
+                    System.out.println(m);
+                    board2[m-holder.getSum()].removeAllCars();
+                    board2[m].setCar(play1,true);
 
 
+                    gui.showMessage("Player2 tryk enter:");
+                    gui.getUserButtonPressed(inputText, new String[]{"Tryk!"});
 
+                    p2+=holder.sum();
+                    gui.setDice(holder.die1.getFacevalue(),holder.die2.getFacevalue());
+                    int n = p2%24;
+                    System.out.println(n);
+                    board2[n-holder.getSum()].removeAllCars();
+                    board2[n].setCar(play2,true);
 
+                    gui.showMessage("Player3 tryk enter:");
+                    gui.getUserButtonPressed(inputText, new String[]{"Tryk!"});
 
-
-
-
-
-
+                    p3+=holder.sum();
+                    gui.setDice(holder.die1.getFacevalue(),holder.die2.getFacevalue());
+                    int b = p3%24;
+                    System.out.println(n);
+                    board2[b-holder.getSum()].removeAllCars();
+                    board2[b].setCar(play3,true);
 
             }
-
-
-
         }
         else {
-            GUI_Car car1 = new GUI_Car();
-            GUI_Player play1 = new GUI_Player("Player1",20,  car1);
+
+
+            GUI_Car car1 = new GUI_Car(Color.RED, Color.RED, GUI_Car.Type.CAR, GUI_Car.Pattern.HORIZONTAL_DUAL_COLOR);
+            GUI_Player play1 = new GUI_Player("Player1",16,  car1);
             gui.addPlayer(play1);
 
-            GUI_Car car2 = new GUI_Car();
-            GUI_Player play2 = new GUI_Player("Player2",20,  car2);
+            GUI_Car car2 = new GUI_Car(Color.YELLOW, Color.WHITE, GUI_Car.Type.UFO, GUI_Car.Pattern.HORIZONTAL_DUAL_COLOR);
+            GUI_Player play2 = new GUI_Player("Player2",16,  car2);
             gui.addPlayer(play2);
 
-            GUI_Car car3 = new GUI_Car();
-            GUI_Player play3 = new GUI_Player("Player2",20,  car2);
+            GUI_Car car3 = new GUI_Car(Color.BLUE, Color.BLACK, GUI_Car.Type.TRACTOR, GUI_Car.Pattern.HORIZONTAL_DUAL_COLOR);
+            GUI_Player play3 = new GUI_Player("Player3",16,  car3);
             gui.addPlayer(play3);
-            GUI_Car car4 = new GUI_Car();
-            GUI_Player play4 = new GUI_Player("Player2",20,  car2);
+            GUI_Car car4 = new GUI_Car(Color.GREEN, Color.BLACK, GUI_Car.Type.RACECAR, GUI_Car.Pattern.HORIZONTAL_DUAL_COLOR);
+            GUI_Player play4 = new GUI_Player("Player4",16,  car4);
             gui.addPlayer(play4);
-
 
             player1 = new Player(16);
             player2= new Player(16);
             player3 = new Player(16);
+            player4 = new Player(16);
             while (playing) {
-                System.out.println(gui.getUserButtonPressed(inputText, new String[]{"Tryk!"}));
-                System.out.println(gui.getUserButtonPressed(inputText, new String[]{"Tryk!"}));
-                holder.sum();
+
+                gui.showMessage("Player1 tryk enter:");
+                gui.getUserButtonPressed(inputText, new String[]{"Tryk!"});
+
+                p1+=holder.sum();
+                gui.setDice(holder.die1.getFacevalue(),holder.die2.getFacevalue());
+                int m=p1%24;
+                System.out.println(m);
+                board2[m-holder.getSum()].removeAllCars();
+                board2[m].setCar(play1,true);
 
 
+                gui.showMessage("Player2 tryk enter:");
+                gui.getUserButtonPressed(inputText, new String[]{"Tryk!"});
 
+                p2+=holder.sum();
+                gui.setDice(holder.die1.getFacevalue(),holder.die2.getFacevalue());
+                int n = p2%24;
+                System.out.println(n);
+                board2[n-holder.getSum()].removeAllCars();
+                board2[n].setCar(play2,true);
 
+                gui.showMessage("Player3 tryk enter:");
+                gui.getUserButtonPressed(inputText, new String[]{"Tryk!"});
 
+                p3+=holder.sum();
+                gui.setDice(holder.die1.getFacevalue(),holder.die2.getFacevalue());
+                int b = p3%24;
+                System.out.println(n);
+                board2[b-holder.getSum()].removeAllCars();
+                board2[b].setCar(play3,true);
+                gui.showMessage("Player4 tryk enter:");
+                gui.getUserButtonPressed(inputText, new String[]{"Tryk!"});
 
-
-
+                p4+=holder.sum();
+                gui.setDice(holder.die1.getFacevalue(),holder.die2.getFacevalue());
+                int v = p4%24;
+                System.out.println(n);
+                board2[v-holder.getSum()].removeAllCars();
+                board2[v].setCar(play4,true);
 
 
 
@@ -165,17 +215,6 @@ public class GameController {
 
 
         }
-
-
-
-
-
-
-
-
-
-
-
 
     }
 
