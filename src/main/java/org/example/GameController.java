6c325b082568bf2fe6c26e85a35945611f5adb7f
Abdisamad Nuh <abdisamad.nuh@outlook.com>
2022-11-21 13:25:56 +0100

    Arbejde på Gamekontroller

--- src/main/java/org/example/GameController.java
+++ src/main/java/org/example/GameController.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import gui_fields.GUI_Car;
 import gui_fields.GUI_Field;
 import gui_fields.GUI_Player;
 import gui_main.GUI;
@@ -9,148 +10,223 @@ import java.util.Scanner;
 
 public class GameController {
     Holder holder;
-boolean Gaming = true;
-
+    boolean Gaming = true;
     Player player1,player2,player3,player4;
-    Player player;
-
-
-
-    Board board2;
+    Board board3;
+    private GameBoard[] board;
+    boolean playing = true;
+    Scanner scanner;
+    String inputText= "";
+    String Textdata;
+    int p1,p2,p3,p4;
+    GUI gui;
+    private GameBoard [] board4;
+    GUI_Field[] board1,board2= new GUI_Field[24];
+    GUI_Field boar;
+    GameBoard gameBoard;
     public GameController(){
         holder=new Holder();
-
-        player1= new Player(20);
-        player2= new Player(20);
-        player3 = new Player(16);
-        player4= new Player(16);
-        board2= new Board();
-        GameBoard gameBoard = new GameBoard(board);
+        board3= new Board();
+        board1 = GameBoard.makeFields();
+        for(int i = 0; i < board2.length; ++i) {
+            board2[i] = board1[i];
+        }
+        gameBoard = new GameBoard(board4);
+        gui = new GUI(board2);
+        scanner = new Scanner(System.in);
     }
-    private GameBoard[] board;
-
-
-    Scanner scanner = new Scanner(System.in);
-    boolean playing = true;
-
-
-    String inputText= "";
-String Textdata;
 
     public void play() {
-        GUI_Field[] board = GameBoard.makeFields();
-        GUI_Field[] board2 = new GUI_Field[24];
-
-        for(int i = 0; i < board2.length; ++i) {
-            board2[i] = board[i];
-        }
-
-        GUI gui = new GUI(board2);
-
-
-
         Textdata = "" + gui.getUserString(inputText);
-        while (playing) {
-
-            System.out.println(gui.getUserButtonPressed(inputText, new String[]{"Tryk!"}));
-            gui.getUserInteger(inputText);
-
-            gui.showMessage("Du har indtastet :" + Textdata);
-            // System.out.println(Textdata);
-            int x = Integer.parseInt(Textdata);
-            System.out.println(x);
+        int x = Integer.parseInt(Textdata);
 
 
-            player1.setPlayerAmount(x,gui,board2);
+        if(x==2) {
+            GUI_Car car1 = new GUI_Car();
+            GUI_Player play1 = new GUI_Player("Player1",20,  car1);
+            gui.addPlayer(play1);
+
+            GUI_Car car2 = new GUI_Car();
+            GUI_Player play2 = new GUI_Player("Player2",20,  car2);
+            gui.addPlayer(play2);
+
+            player1 = new Player(20);
+            player2= new Player(20);
+            board2[0].setCar(play1,true);
+            board2[0].setCar(play2,true);
+            while (playing) {
+                gui.showMessage("Player1 tryk enter:");
+                gui.getUserButtonPressed(inputText, new String[]{"Tryk!"});
+
+                p1+=holder.sum();
+                gui.setDice(holder.die1.getFacevalue(),holder.die2.getFacevalue());
+               int m=p1%24;
+                System.out.println(m);
+                board2[m-holder.getSum()].removeAllCars();
+                board2[m].setCar(play1,true);
+
+                //board2[m].setCar(play1,true);
 
 
+                gui.showMessage("Player2 tryk enter:");
+                gui.getUserButtonPressed(inputText, new String[]{"Tryk!"});
 
+                p2+=holder.sum();
+                gui.setDice(holder.die1.getFacevalue(),holder.die2.getFacevalue());
+                int n = p2%24;
+                System.out.println(n);
+                board2[n-holder.getSum()].removeAllCars();
+                board2[n].setCar(play2,true);
 
-            holder.sum();
-            gui.setDice(holder.die1.getFacevalue(),holder.die2.getFacevalue());
-            System.out.println(holder.getSum());
-            player1.moveSquare(holder.getSum());
-
-            player1.moveCar1(board2,holder.getSum());
-
-            gui.getUserButtonPressed("ja", String.valueOf(true));
-            //player2.setPlayerAmount(x,gui,board2);
-            //holder.sum(gui);
-            //NUVÆRENDE BUG er at player1 bliver sat som setPlayerAmount
-            //player2.moveCar2(board2,holder.getSum());
-            //player2.moveCar2(board2,holder.getSum());
-            //gui.getUserButtonPressed("ja", String.valueOf(true));
-            //player2.moveCar2(board2,holder.getSum());
-
-            //player1.moveCar(gui,board2,holder.sum(gui));
-
-           /*
-                        for(int i = 0; i <  parseTextdata; ++i) {
-                        }
-                  //  }
-                //} catch (Exception e) {
-                  //  System.out.println("Den er helt gakgak");
-*/
-
-
+                //board2[n].setCar(play2,true);
 
 
 
 
-            updateFlow();
-            while (holder.getSum() == 10) {
-                System.out.println("Du får et ekstra slag");
-                updateFlow();
 
-            }
-            if (player1.myWallet.getMoney() >= 3000) {
-                playing = false;
-                System.out.println("Spiller 1 har vundet");
-                break;
-            }
 
-            updateFlow2();
-            while (holder.getSum() == 10) {
-                System.out.println("Du får et ekstra slag");
-                updateFlow2();
 
-            }
-            if (player2.myWallet.getMoney() >= 3000) {
-                playing = false;
-                System.out.println("Spiller 2 har vundet");
-                break;
+
+
+
+
             }
         }
+        else if (x==3){
+            GUI_Car car1 = new GUI_Car();
+            GUI_Player play1 = new GUI_Player("Player1",20,  car1);
+            gui.addPlayer(play1);
+
+            GUI_Car car2 = new GUI_Car();
+            GUI_Player play2 = new GUI_Player("Player2",20,  car2);
+            gui.addPlayer(play2);
+
+            GUI_Car car3 = new GUI_Car();
+            GUI_Player play3 = new GUI_Player("Player2",20,  car2);
+            gui.addPlayer(play3);
+
+            player1 = new Player(18);
+            player2= new Player(18);
+            player3 = new Player(18);
+            while (playing) {
+                System.out.println(gui.getUserButtonPressed(inputText, new String[]{"Tryk!"}));
+
+
+
+
+
+
+
+
+
+
+
+            }
+
+
+
+        }
+        else {
+            GUI_Car car1 = new GUI_Car();
+            GUI_Player play1 = new GUI_Player("Player1",20,  car1);
+            gui.addPlayer(play1);
+
+            GUI_Car car2 = new GUI_Car();
+            GUI_Player play2 = new GUI_Player("Player2",20,  car2);
+            gui.addPlayer(play2);
+
+            GUI_Car car3 = new GUI_Car();
+            GUI_Player play3 = new GUI_Player("Player2",20,  car2);
+            gui.addPlayer(play3);
+            GUI_Car car4 = new GUI_Car();
+            GUI_Player play4 = new GUI_Player("Player2",20,  car2);
+            gui.addPlayer(play4);
+
+
+            player1 = new Player(16);
+            player2= new Player(16);
+            player3 = new Player(16);
+            while (playing) {
+                System.out.println(gui.getUserButtonPressed(inputText, new String[]{"Tryk!"}));
+                System.out.println(gui.getUserButtonPressed(inputText, new String[]{"Tryk!"}));
+                holder.sum();
+
+
+
+
+
+
+
+
+
+
+
+            }
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
     }
 
-    public void PlayerChecker() {
+
+
+
+
+
+
+
+
+    private void updateFlow1(){
+        gui.showMessage("Tryk enter for at slå terningerne");
+        System.out.println(gui.getUserButtonPressed(inputText, new String[]{"Tryk!"}));
+        holder.sum();
+
     }
 
-    public void updateFlow(){
-        System.out.println("Tryk på enter spiller 1");
-        String p1 = scanner.nextLine();
-        //holder.sum();
 
-        Comment();
-        System.out.println(" player 1 har nu "+player1.myWallet.UpdateMoney()+" ");
-    }
-
-
-    public void updateFlow2(){
+    private void updateFlow2(){
         System.out.println("Tryk på enter spiller 2");
         String p2 = scanner.nextLine();
         System.out.println();
         //holder.sum();
 
-        Comment();
+        System.out.println(" player 2 har nu "+player2.myWallet.UpdateMoney()+" ");
+
+    }
+    private void updateFlow3(){
+        System.out.println("Tryk på enter spiller 2");
+        String p2 = scanner.nextLine();
+        System.out.println();
+        //holder.sum();
+
+
+        System.out.println(" player 2 har nu "+player2.myWallet.UpdateMoney()+" ");
+
+    }
+    private void updateFlow4(){
+        System.out.println("Tryk på enter spiller 2");
+        String p2 = scanner.nextLine();
+        System.out.println();
+        //holder.sum();
+
+
         System.out.println(" player 2 har nu "+player2.myWallet.UpdateMoney()+" ");
 
     }
 
-    public void Comment(){
 
-        }
-    }
+}
 
 
 
