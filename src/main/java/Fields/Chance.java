696886e5794e5546ad19c53f87656a6d0542d38f
Casper H. Blichfeldt <Casper.h.blichfeldt@live.dk>
2022-11-23 20:39:53 +0100

    Chancekort printer når man rammer chacefelt

--- src/main/java/Fields/Chance.java
+++ src/main/java/Fields/Chance.java
@@ -4,12 +4,15 @@ import org.example.Player;
 import org.example.Wallet;
 
 public class Chance extends UnbuyableField{
+    Chancekort chance = new Chancekort();
     public Chance (String fieldName, int BoardNumber){
         super(fieldName,BoardNumber);
+        //System.out.println(chance.traekKort());
 
     }
     @Override
     public void landOndField(Player player1,Player player2) {
+        System.out.println(chance.traekKort());
          /*
          if(player1.isInJail()) {
             player1.myWallet.setSquareMoney(-1);
