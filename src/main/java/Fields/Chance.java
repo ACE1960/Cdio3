1d4bb9d385e2f76a9c6ada60407ea310b4df57f3
Casper H. Blichfeldt <Casper.h.blichfeldt@live.dk>
2022-11-25 22:16:43 +0100

    Hej

--- src/main/java/Fields/Chance.java
+++ src/main/java/Fields/Chance.java
@@ -1,18 +1,29 @@
 package Fields;
 
 import org.example.Player;
-import org.example.Wallet;
 
 public class Chance extends UnbuyableField{
-    Chancekort chance = new Chancekort();
+    private Chancekort chance = new Chancekort();
+    private String Beskrivelse;
+
     public Chance (String fieldName, int BoardNumber){
         super(fieldName,BoardNumber);
         //System.out.println(chance.traekKort());
 
     }
+
+    public String setChance(){
+        this.Beskrivelse = chance.traekKort();
+        return chance.traekKort();
+    }
+
     @Override
     public void landOndField(Player player1,Player player2) {
-        System.out.println(chance.traekKort());
+        System.out.println(setChance());
+        if(setChance().equals("Ryk frem til START. Modtag M2.")){
+            System.out.println("2");
+        }
+
          /*
          if(player1.isInJail()) {
             player1.myWallet.setSquareMoney(-1);
